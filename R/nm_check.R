c0211383e0e5eed787531bc303125f054ded43fb
Patrick Nolain <patrick.nolain@sanofi.com>
2019-02-12 11:12:00 +0100

    merge pmxecute

---
+++ R/nm_check.R
@@ -0,0 +1,58 @@
+#' Performs a NM-TRAN check of the control file
+#'
+#' @param control_file Path of the control file
+#' @param nmcheck_cmd Path of NMTRAN check executable
+#'
+#' @return A list with control file path and error/error message if any.
+#' @export
+#'
+#' @examples
+nm_check <- function(control_file,
+                     nmcheck_exe = NULL,
+                     call_template = NULL) {
+
+  stopifnot(!is.null(nmcheck_exe))
+  stopifnot(file.exists(control_file))
+
+  if(!file.exists(nmcheck_exe))
+    stop(simpleError("`nmcheck_exe` not found."))
+
+  if(is.null(call_template) || call_template == "")
+    call_template <- "cat {control_file} | {nmcheck_exe}"
+
+  owd <- setwd(dirname(control_file))
+  on.exit(setwd(owd))
+
+  #safe_call <- safely(~ system2("cat", args = sprintf("%s | %s", basename(control_file), nmcheck_cmd), stdout = TRUE))
+  # check_cmd <- str_c(nmcheck_cmd, basename(control_file), sep = " ")
+  nmcheck_cmd <- glue::glue(call_template)
+
+  safe_call <- safely(~ system(nmcheck_cmd, intern = TRUE))
+
+  nmtran_check <- safe_call()
+
+  nmtran_files <- c("FCON", "FDATA", "FREPORT", "FSIZES", "FSTREAM", "FSUBS", "FSUBS2", "FSUBS_MU.F90", "FWARN", "PRSIZES.f90", "trash.tmp")
+
+  nmtran_files %>% walk(~if(file.exists(.)) file.remove(.))
+
+  is_error <- FALSE
+  msg <- NULL
+
+  if(!is.null(nmtran_check$error)){
+    is_error <- TRUE
+
+    msg <- nmtran_check$error
+
+  } else {
+    msg <- nmtran_check$result %>%
+      stringr::str_conv("") %>%
+      str_trim() %>%
+      str_c(collapse = "\n")
+
+    is_error <- str_detect(msg, "AN ERROR WAS FOUND IN THE CONTROL STATEMENTS.")
+  }
+
+  list(path = control_file,
+       error = is_error,
+       message = msg)
+}
---
+++ R/nm_exec2.R
@@ -0,0 +1,25 @@
+#' Starts a NONMEM run and do not wait for the results
+#'
+#' Starts a NONMEM run and do not wait for the results. Useful when NONMEM jobs are run via an HPC scheduler or a cloud solution.
+#'
+#' @param control_file path of the control file
+#' @param call_template character string call template used to start the run.
+#'
+#' @return A character string returned by the system call.
+#' @export
+#'
+#' @examples
+nm_exec2 <- function(control_file,
+                     call_template = NULL,
+                     ...) {
+
+  if(!file.exists(control_file))
+    stop(simpleError("Control file not found."))
+
+  if(is.null(call_template) || call_template == "")
+    stop(simpleError("You must specify a call template to start the remote NONMEM run."))
+
+  cmd <- glue::glue(call_template)
+
+  system(cmd, intern = TRUE)
+}
